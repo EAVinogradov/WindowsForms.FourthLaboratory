c277b6e008df6c07bf8a94c2faff150668759163
EAVinogradov <e.a.vinogradov@icloud.com>
2020-06-03 18:29:24 +0300

    Добавьте файлы проекта.

---
+++ EAVinogradov.CSWindowsForms.FourthLaboratory.SecondExample/EditPerson.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace EAVinogradov.CSWindowsForms.FourthLaboratory.SecondExample
+{
+    partial class EditPerson
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(516, 112);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.VirtualMode = true;
+            this.listView1.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.listView1_RetrieveVirtualItem);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Редактировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Фамилия";
+            this.columnHeader2.Width = 271;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Возраст";
+            this.columnHeader3.Width = 126;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 200);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(515, 125);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(252, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Просмотреть список";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // EditPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 352);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "EditPerson";
+            this.Text = "Список сотруднкиов";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ EAVinogradov.CSWindowsForms.FourthLaboratory.SecondExample/EditPerson.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAVinogradov.CSWindowsForms.FourthLaboratory.SecondExample
+{
+    public partial class EditPerson : Form
+    {
+        
+        public EditPerson()
+        {
+            InitializeComponent();
+        }
+        List<Person> pers = new List<Person>();
+        public void button1_Click(object sender, EventArgs e)
+        {
+            Person personal = new Person();
+            EditPersonForm editForm = new EditPersonForm(personal);
+            if (editForm.ShowDialog() != DialogResult.OK)
+                return;
+            pers.Add(personal);
+            listView1.VirtualListSize = pers.Count;
+            listView1.Invalidate();
+            //ListViewItem newItem = personsListView.Items.Add(editForm.FirstName); 
+            //newItem.SubItems.Add(editForm.LastName); 
+            //newItem.SubItems.Add(editForm.Age.ToString());
+
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            //if (personsListView.SelectedItems.Count == 0)
+                //return;
+            
+            Person personal = pers[listView1.SelectedIndices[0]];
+            //ListViewItem item = personsListView.SelectedItems[0];
+            EditPersonForm editForm = new EditPersonForm(personal);
+            //editForm.FirstName = item.Text; editForm.LastName = item.SubItems[1].Text;
+            //editForm.Age = Convert.ToInt32(item.SubItems[2].Text);
+            if (editForm.ShowDialog() != DialogResult.OK)
+            {
+                listView1.Invalidate();
+            }
+                //return;
+            //item.Text = editForm.FirstName; 
+            //item.SubItems[1].Text = editForm.LastName; 
+            //item.SubItems[2].Text = editForm.Age.ToString();
+
+        }
+
+        public void listView1_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            if (e.ItemIndex >= 0 && e.ItemIndex < pers.Count)
+            {
+                e.Item = new ListViewItem(pers[e.ItemIndex].FirstName); 
+                e.Item.SubItems.Add(pers[e.ItemIndex].LastName); 
+                e.Item.SubItems.Add(pers[e.ItemIndex].Age.ToString());
+            }
+
+        }
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(); foreach (Person item in pers)
+            {
+                sb.Append("Сотрудник: \n" + item.ToString());
+            }
+            richTextBox1.Text = sb.ToString();
+        }
+    }
+}
